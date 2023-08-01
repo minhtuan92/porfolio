ff967677ebeeaa2fd3fd44ce038bba9b0aa2c262
Tuan Tran <tminhtuan@tma.com.vn>
2023-08-01 18:34:39 +0700

    update

--- src/app/shared/layouts/components/header/header.component.ts
+++ src/app/shared/layouts/components/header/header.component.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { RouterLink } from '@angular/router'
 
+interface Navigation {
+  label?: string
+  link?: string
+}
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  navigations: Navigation[] = [
+    {
+      label: '_home',
+      link: ''
+    },
+    {
+      label: '_about',
+      link: ''
+    },
+    {
+      label: '_skills',
+      link: ''
+    },
+    {
+      label: '_resume',
+      link: ''
+    },
+    {
+      label: '_projects',
+      link: ''
+    }
+  ]
+}
--- src/app/shared/layouts/main-layout/main-layout.component.ts
+++ src/app/shared/layouts/main-layout/main-layout.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { RouterOutlet } from '@angular/router'
+
 import { SidebarComponent } from '../components/sidebar/sidebar.component'
 import { HeaderComponent } from '../components/header/header.component'
+import { FooterComponent } from '../components/footer/footer.component'
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [CommonModule, SidebarComponent, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, SidebarComponent, HeaderComponent, FooterComponent],
   templateUrl: './main-layout.component.html',
   styleUrls: ['./main-layout.component.css']
 })

0f09eaefd7d96295e26ea7462cfe4067ad26cb88
Tuan Tran <minhtuan92vt@gmail.com>
2023-12-04 00:59:18 +0700

    update code

--- src/app/features/resume/resume.component.ts
+++ src/app/features/resume/resume.component.ts
@@ -1,13 +1,12 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { AngularSvgIconModule } from 'angular-svg-icon'
 
 @Component({
   selector: 'app-resume',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AngularSvgIconModule],
   templateUrl: './resume.component.html',
   styleUrls: ['./resume.component.css']
 })
-export class ResumeComponent {
-
-}
+export class ResumeComponent {}

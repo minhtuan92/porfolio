dae339e6d98c1f2bdf1220975f15aabc76f2f0e5
Tuan Tran <minhtuan92vt@gmail.com>
2024-02-19 15:28:42 +0700

    update code
    Signed-off-by: Tuan Tran <tminhtuan@tma.com.vn>

---
+++ src/app/core/services/theme/theme.service.ts
@@ -0,0 +1,51 @@
+import { ApplicationRef, Injectable, OnInit, Optional, SkipSelf } from '@angular/core'
+
+import { EnsureLoadedOnceGuard } from 'src/app/shared/utils'
+import { LOCALSTORAGE_ITEMS, THEME_OPTIONS } from 'src/app/shared/constants'
+import { BehaviorSubject, Subject } from 'rxjs'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ThemeService extends EnsureLoadedOnceGuard {
+  themeChange$ = new Subject<void>()
+  currentTheme: string
+  constructor(
+    @Optional() @SkipSelf() parent: ThemeService,
+    private ref: ApplicationRef
+  ) {
+    super(parent)
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
+      this.updateTheme()
+      this.themeChange$.next()
+
+      // Trigger refresh of UI
+      this.ref.tick()
+    })
+  }
+
+  updateTheme() {
+    const theme = localStorage.getItem(LOCALSTORAGE_ITEMS.THEME) || ''
+    if (theme === THEME_OPTIONS.LIGHT) {
+      this.removeDarkClass()
+      this.currentTheme = THEME_OPTIONS.LIGHT
+    } else if (theme === THEME_OPTIONS.SYSTEM) {
+      if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        this.addDarkClass()
+      } else this.removeDarkClass()
+      this.currentTheme = THEME_OPTIONS.SYSTEM
+    } else {
+      this.addDarkClass()
+      if (!theme) localStorage.setItem(LOCALSTORAGE_ITEMS.THEME, THEME_OPTIONS.DARK)
+      this.currentTheme = THEME_OPTIONS.DARK
+    }
+  }
+
+  addDarkClass() {
+    document.documentElement.classList.add('dark')
+  }
+
+  removeDarkClass() {
+    document.documentElement.classList.remove('dark')
+  }
+}
---
+++ src/app/shared/constants/theme-options.constant.ts
@@ -0,0 +1,5 @@
+export const THEME_OPTIONS = {
+  DARK: 'dark',
+  LIGHT: 'light',
+  SYSTEM: 'system'
+}

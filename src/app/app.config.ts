14fa5daba00d2f1a67c08ea0dcc34f241ad81daa
Tuan Tran <minhtuan92vt@gmail.com>
2024-02-19 15:28:46 +0700

    update code
    Signed-off-by: Tuan Tran <tminhtuan@tma.com.vn>

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,16 @@
-import { ApplicationConfig } from '@angular/core'
+import { ApplicationConfig, importProvidersFrom } from '@angular/core'
 import { provideRouter } from '@angular/router'
 
 import { routes } from './app.routes'
 import { ThemeService } from './core/services'
+import { AngularSvgIconModule } from 'angular-svg-icon'
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), ThemeService]
+  providers: [
+    importProvidersFrom(AngularSvgIconModule.forRoot()),
+    provideHttpClient(withInterceptorsFromDi()),
+    provideRouter(routes),
+    ThemeService
+  ]
 }
--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
-
+import { SkeletonModule } from 'primeng/skeleton'
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SkeletonModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {}
+export class HomeComponent {
+  isMyImageLoading = true
+}

fefbf714b0fb0b1c53cf581c49e90df173845371
Tuan Tran <minhtuan92vt@gmail.com>
2024-02-19 15:29:04 +0700

    update code

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,6 +25,9 @@ module.exports = {
         gallery: '#EBEBEB',
         concrete: 'F3F3F3'
       },
+      boxShadow: {
+        xs: '0px 1px 2px 0px rgba(16, 24, 40, 0.05)'
+      },
       keyframes: {
         fadein: {
           '0%': {

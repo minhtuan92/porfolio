f0d1dfaa34bc3b38ca4f221a34334a4f865a8229
Tuan Tran <minhtuan92vt@gmail.com>
2024-02-19 15:28:47 +0700

    update code

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { SkeletonModule } from 'primeng/skeleton'
+import { AnimateModule } from 'primeng/animate'
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, SkeletonModule],
+  imports: [CommonModule, SkeletonModule, AnimateModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })

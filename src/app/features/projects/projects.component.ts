84a764b5ef05a168e096a2db15eb2293e41769b9
Tuan Tran <minhtuan92vt@gmail.com>
2024-03-25 11:36:54 +0700

    Update code
    Signed-off-by: Tuan Tran <minhtuan92vt@gmail.com>

--- src/app/features/projects/projects.component.ts
+++ src/app/features/projects/projects.component.ts
@@ -21,7 +21,9 @@ export class ProjectsComponent implements OnInit {
         name: 'Formbuilder Application - Technical Lead',
         shortName: 'Formbuilder',
         desc: `Description: This is an application to build form from JSON scheme.
+
               Responsibilities: discuss with clients to clarify their high-level design, perform front development activities, troubleshoot and handle technical blocker issues.
+              
               Technologies: angular, bootstrap, tailwindCSS, typescript, SCSS, sonarqube, trivy, bearer`
       },
       {
@@ -29,7 +31,9 @@ export class ProjectsComponent implements OnInit {
         name: 'Travel Management System - Frontend Developer',
         shortName: 'Travel Assistant',
         desc: `Description: An application to manage travel tour, social network, e-commercial.
+              
               Responsibilities: perform front development activities, troubleshoot and handle technical blocker issues.
+              
               Technologies: angular, tailwindCSS, springboot, typescript, java, cassandra, postgreSQL, mongoDB, kafka, redis...`
       },
       {
@@ -37,7 +41,9 @@ export class ProjectsComponent implements OnInit {
         name: 'BUSSINESS WORKFLOW MANAGEMENT SYSTEM - Technical Lead',
         shortName: 'Dynamic Workflow',
         desc: `Description: A system to manage company business activities include modules: Dynamic Workflow using BPMN, Form Management, Task Management, ...
+              
               Responsibilities: perform front development activities, troubleshoot and handle technical blocker issues.
+              
               Technologies: angular, tailwindCSS, springboot, typescript, java, cassandra, postgreSQL, mongoDB, kafka, redis...`
       }
     ]
